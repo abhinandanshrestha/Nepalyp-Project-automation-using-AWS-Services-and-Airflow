f2be5a283ee47666422232117cfff90402401777
Abhinandan Shrestha <shtabhi@gmail.com>
2023-05-10 18:26:42 +0545

    Defined item container

--- finalquestion/items.py
+++ finalquestion/items.py
@@ -3,10 +3,24 @@
 # See documentation in:
 # https://docs.scrapy.org/en/latest/topics/items.html
 
+# I want to use PostgreSQL as my database
+# I have to follow the following steps
+# Scraped data --> Item containers --> Store in the database (Postgresql)
+
 import scrapy
 
 
 class FinalquestionItem(scrapy.Item):
     # define the fields for your item here like:
     # name = scrapy.Field()
-    pass
+    category = scrapy.Field()
+    rating = scrapy.Field()
+    no_of_reviews = scrapy.Field()
+    company_name = scrapy.Field()
+    district_location = scrapy.Field()
+    phone_no = scrapy.Field()
+    website = scrapy.Field()
+    working_hour = scrapy.Field()
+    establishment_year = scrapy.Field()
+    no_of_employee = scrapy.Field()
+    company_manager = scrapy.Field()
--- finalquestion/spiders/scrape.py
+++ finalquestion/spiders/scrape.py
@@ -1,10 +1,11 @@
 import scrapy
+from ..items import FinalquestionItem
 
 class ScrapeSpider(scrapy.Spider):
     name = 'scrape'
     # allowed_domains = ['nepalyp.com']
-    categories=['Communications','Web_Design','Computer_Consumables','Computer_services','Web_development','Computer_software_solution','Computer_training','Internet_service_providers','Web_services','Web_hosting','Software_applications','Networking','Information_technology','Online_Content','Computers_hardware','Computer_repair','Mail_Services']
-    # categories=['Communications','Web_Design']
+    # categories=['Communications','Web_Design','Computer_Consumables','Computer_services','Web_development','Computer_software_solution','Computer_training','Internet_service_providers','Web_services','Web_hosting','Software_applications','Networking','Information_technology','Online_Content','Computers_hardware','Computer_repair','Mail_Services']
+    categories=['Web_Design']
     # start_urls = ['http://nepalyp.com/category/Communications/']
     # base_url='http://www.nepalyp.com'
 
@@ -27,6 +28,9 @@ class ScrapeSpider(scrapy.Spider):
             yield response.follow(link.get(),callback=self.parsecat, meta={'category': category})
 
     def parsecat(self,response):
+
+        items=FinalquestionItem()
+
         # info_list=response.css('div.info::text').getall()
         category = response.meta['category']
         # Extract Working Hours
@@ -45,16 +49,30 @@ class ScrapeSpider(scrapy.Spider):
         manager_label = response.xpath('//span[@class="label" and contains(text(), "Company manager")]/following-sibling::text()').get()
         manager = manager_label.strip() if manager_label is not None else 'Not Specified'
 
-        yield{
-            'category': category,
-            'rating':response.css('div.cmp_rate span.rate::text').get(),
-            'no_of_reviews':response.css('div.cmp_rate a.reviews_count span::text').get(),
-            'company_name':response.css('#company_name::text').get(),
-            'district_location':response.css('.location a::text').get(),
-            'phone_no':response.css('.phone::text').get(),
-            'website':response.css('div.weblinks a::attr(href)').get(),
-            'working_hour':working_hours,
-            'establishment_year':establishment_year,
-            'no_of_employess':employees,
-            'company_manager':manager
-        }
+
+        items['category']=category
+        items['rating']=response.css('div.cmp_rate span.rate::text').get()
+        items['no_of_reviews']=response.css('div.cmp_rate a.reviews_count span::text').get()
+        items['company_name']=response.css('#company_name::text').get()
+        items['district_location']=response.css('.location a::text').get()
+        items['phone_no']=response.css('.phone::text').get()
+        items['website']=response.css('div.weblinks a::attr(href)').get()
+        items['working_hour']=working_hours
+        items['establishment_year']=establishment_year
+        items['no_of_employee']=employees
+        items['company_manager']=manager
+
+        yield items
+        # yield{
+        #     'category': category,
+        #     'rating':response.css('div.cmp_rate span.rate::text').get(),
+        #     'no_of_reviews':response.css('div.cmp_rate a.reviews_count span::text').get(),
+        #     'company_name':response.css('#company_name::text').get(),
+        #     'district_location':response.css('.location a::text').get(),
+        #     'phone_no':response.css('.phone::text').get(),
+        #     'website':response.css('div.weblinks a::attr(href)').get(),
+        #     'working_hour':working_hours,
+        #     'establishment_year':establishment_year,
+        #     'no_of_employess':employees,
+        #     'company_manager':manager
+        # }
